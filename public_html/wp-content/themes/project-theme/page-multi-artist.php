69ba525f3d2d96208b6990ad774894d76b2b81b4
Errol Sidelsky <errol@squie.com>
2020-02-21 14:42:12 +0000

    Multi artist template created

---
+++ public_html/wp-content/themes/project-theme/page-multi-artist.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Multi artist
+ */
+
+	use App\Helper\Render;
+	$render = new Render;
+	include("header.php");
+	/* Include login modal */
+	include("login.php");
+
+?>
+
+
+
+<?php include("footer.php"); ?>
