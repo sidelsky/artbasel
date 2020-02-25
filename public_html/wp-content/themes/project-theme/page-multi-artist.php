3a18647b304a674527ecd1ba750ef758e2dda5f5
Errol Sidelsky <errol@squie.com>
2020-02-25 12:57:21 +0000

    Max width mixin

--- public_html/wp-content/themes/project-theme/page-multi-artist.php
+++ public_html/wp-content/themes/project-theme/page-multi-artist.php
@@ -15,11 +15,13 @@ $allLayouts = $layout->getLayout();
 include("header.php"); ?>
 
 <section class="c-ma-email-sub u-l-horizontal-padding--large u-l-vertical-padding--small">
-    <div class="c-ma-email-sub__column">
-        <span>Be the first to receive updates on<br>Artists Choices Online Viewing Room presentations</span>
-    </div>
-    <div class="c-ma-email-sub__column">
-        <?php echo do_shortcode('[gravityform id="4" title="false" description="false"]'); ?>
+    <div class="c-ma-email-sub__wrapper">
+        <div class="c-ma-email-sub__column">
+            <span>Be the first to receive updates on<br>Artists Choices Online Viewing Room presentations</span>
+        </div>
+        <div class="c-ma-email-sub__column">
+            <?php echo do_shortcode('[gravityform id="4" title="false" description="false"]'); ?>
+        </div>
     </div>
 </section>
     
