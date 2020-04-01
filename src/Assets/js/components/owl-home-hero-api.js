5b4bf896fd41ed3a1b6a22f51d761ff1e1ddb573
Errol Sidelsky <errol@squie.com>
2020-04-01 23:13:40 +0100

    css tests

--- src/Assets/js/components/owl-home-hero-api.js
+++ src/Assets/js/components/owl-home-hero-api.js
@@ -1,30 +1,33 @@
-$(document).ready(function(){
-    $('.owl-carousel-home').owlCarousel({
-        center: true,
-        dots: false,
-        nav: true,
-        responsiveClass: true,
-        lazyLoad: true,
-        loop: true,
-        margin: 20,
-        autoHeight: false,
-        navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
-        responsive:{
-            0: {
-                items: 1,
-              },
-          
-              600: {
-                items: 1
-              },
-          
-              1024: {
-                items: 1
-              },
-          
-              1366: {
-                items: 1
-              }
-        }
-    });
+$(document).ready(function() {
+  $(".owl-carousel-home").owlCarousel({
+    margin: 20,
+    center: true,
+    dots: false,
+    nav: true,
+    responsiveClass: true,
+    lazyLoad: true,
+    loop: true,
+    autoHeight: false,
+    navText: [
+      "<div class='nav-btn prev-slide'></div>",
+      "<div class='nav-btn next-slide'></div>"
+    ],
+    responsive: {
+      0: {
+        items: 1
+      },
+
+      600: {
+        items: 1
+      },
+
+      1024: {
+        items: 1
+      },
+
+      1366: {
+        items: 1
+      }
+    }
   });
+});
