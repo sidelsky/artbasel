97dae37e3da5586caf65e92ebdb5c153efb84968
Errol Sidelsky <errol@squie.com>
2020-04-16 10:42:09 +0100

    dev & master test

--- src/Assets/js/components/paralax.js
+++ src/Assets/js/components/paralax.js
@@ -452,6 +452,6 @@ $(window).on("resize", function(e) {
   clearTimeout(resizeTimer);
   resizeTimer = setTimeout(function() {
     $(".parallax-window").parallax("refresh");
-    console.log("Refresh");
+    console.log("Refresh test");
   }, 250);
 });
