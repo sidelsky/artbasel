11e1965a773f7b86c8a519badafe99067463e283
Errol Sidelsky <errol@squie.com>
2020-06-16 16:31:56 +0100

    Spacing fixes

--- public_html/wp-content/themes/project-theme/single-online-exhibitions.php
+++ public_html/wp-content/themes/project-theme/single-online-exhibitions.php
@@ -185,8 +185,8 @@ $kuulaVr = $fiftyFifty['fifty_fifty_kuula'];
 $kuulaImage = $fiftyFifty['kuula_image']['sizes']['large'];
 
 if( $kuulaImage && $kuulaVr ) : ?>
-	<section class="u-section">
-		<div class="c-kuula">
+	<section class="u-section" style="margin-bottom: 30px"> 
+		<div class="c-kuula ">
 			<button class="c-kuula__button" id="touchButton" onclick="touchFunction()">
 				<svg class="c-kuula__touch-icon">
 					<use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-touch" viewBox="0 0 32 32"></use>
