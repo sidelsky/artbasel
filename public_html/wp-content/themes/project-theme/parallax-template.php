34be2986f47abb974b63ef28c8158a164c4469c7
Errol Sidelsky <errol@squie.com>
2020-09-04 18:48:33 +0100

    Video with caption

--- public_html/wp-content/themes/project-theme/parallax-template.php
+++ public_html/wp-content/themes/project-theme/parallax-template.php
@@ -246,7 +246,35 @@ if( $parallaxVideo ): ?>
 									$alt = $row['magnify_carousel_image']['alt'];
 									$image_magnify = $row['magnify_carousel_image']['sizes']['large'];
 									$caption = $row['magnify_carousel_image']['caption'];
-									$video = $row['magnify_carousel_video'];
+
+									//$iFrame = $row['magnify_carousel_video'];
+
+									// Get iframe HTML
+									$vimeo_iframe = $row['magnify_carousel_video'];
+
+									// use preg_match to find iframe src
+									preg_match('/src="(.+?)"/', $vimeo_iframe, $matches);
+									$src = $matches[1];
+
+									// add extra params to iframe src
+									$params = array(
+										'controls'  => 0,
+										'hd'        => 1,
+										'autohide'  => 0,
+										'loop' 		=> 1,
+										'title'		=> 0,
+										'url' 		=> 1
+									);
+
+									$new_src = add_query_arg($params, $src);
+
+									$vimeo_iframe = str_replace($src, $new_src, $vimeo_iframe);
+
+									// add extra attributes to iframe html
+									$attributes = 'frameborder="0"';
+
+									$vimeo_iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $vimeo_iframe);
+									
 
 									// Check if magnifying glass is active
 									if ( $show_magnifying_glass ) {
@@ -254,12 +282,15 @@ if( $parallaxVideo ): ?>
 									}
 
 
-									if ( $video ) {
-										
+									if ( $vimeo_iframe ) {
+
 										echo '<div class="c-video-player--centered " id="video" data-id="video">';
-											echo '<button class="c-video-player__button" data-id="playBtn"><svg class="c-video-player__play-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-play" viewBox="0 0 32 32"></use></svg></button>';
-											echo '<figure class="c-video-player__cover-image" role="img" aria-label="' . esc_attr( $alt ) . '" style="background-image: url('. $image .')" data-id="cover"></figure>';
-											echo '<div data-id="vimeo-content" class="u-video-aspect-ratio">' . $video . '</div>';
+										echo '<button class="c-video-player__button" data-id="playBtn"><svg class="c-video-player__play-icon"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-play" viewBox="0 0 32 32"></use></svg></button>';
+											echo '<figure class="c-paralax-carousel-cover" data-id="cover">';
+												echo '<img class="c-paralax-carousel-cover__image" src="' . $image . '" alt="' . $alt . '">';
+												echo '</figure>';
+												echo '<div data-id="vimeo-content" class="u-video-aspect-ratio">' . $vimeo_iframe  . '</div>';
+												echo '<figcaption class="caption">' . $caption . '</figcaption>';
 										echo '</div>';
 														
 									} else {
