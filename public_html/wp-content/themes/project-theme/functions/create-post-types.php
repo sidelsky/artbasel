77c0fdfa617890954849ad7fc070ad3825187fef
Errol Sidelsky <errol@squie.com>
2020-09-21 20:20:00 +0100

    Gallery Exhibitions CPT created

--- public_html/wp-content/themes/project-theme/functions/create-post-types.php
+++ public_html/wp-content/themes/project-theme/functions/create-post-types.php
@@ -20,10 +20,10 @@
                 'menu_icon' =>  'dashicons-format-image',
                 'menu_position' => 5,
                 'supports' => [
-                    'title',
-                    'editor',
-                    'thumbnail',
-                    'excerpt',
+					'title',
+					'editor',
+					'thumbnail',
+					'excerpt',
 					'revisions'
 						],
 					]);
@@ -38,11 +38,11 @@
 					'with_front' => false
 				],
 				'menu_icon' =>  'dashicons-format-gallery',
-				'menu_position' => 6,
+				'menu_position' => 5,
 				'supports' => [
-                    'title',
-                    'thumbnail',
-                    'editor',
+					'title',
+					'thumbnail',
+					'editor',
 					'revisions'
 				],
 			]);
@@ -57,69 +57,88 @@
 					'with_front' => false
 				],
 				'menu_icon' =>  'dashicons-images-alt2',
-				'menu_position' => 7,
-				'supports' => [
-                    'title',
-                    'thumbnail',
-                    'editor',
-					'revisions'
-				],
-			]);
-
-
-			// On view
-        	create_post_type([
-				'name' => 'On view',
-				'singular_name' => 'On view',
-				'has_archive' => false,
-				'rewrite' => [
-				//'slug' => 'private-sales/private-rooms',
-				'with_front' => false
-				],
-				'menu_icon' =>  'dashicons-admin-site-alt2',
-				'menu_position' => 8	,
+				'menu_position' => 5,
 				'supports' => [
 					'title',
 					'thumbnail',
+					'editor',
 					'revisions'
 				],
 			]);
 
-		   // Private sales
+			// Gallery Exhibitions
         	create_post_type([
-				'name' => 'Private sales',
-				'singular_name' => 'Private sale',
+				'name' => 'Gallery exhibitions',
+				'singular_name' => 'Gallery exhibitions',
 				'has_archive' => false,
 				'rewrite' => [
-					//'slug' => 'private-sales/private-rooms',
+					//'slug' => 'private-sales/private-room',
 					'with_front' => false
 				],
-				'menu_icon' =>  'dashicons-welcome-view-site',
-				'menu_position' => 9,
+				'menu_icon' =>  'dashicons-images-alt',
+				'menu_position' => 5,
 				'supports' => [
 					'title',
 					'thumbnail',
+					'editor',
 					'revisions'
 				],
 			]);
 
+			// On view
+        	// create_post_type([
+			// 	'name' => 'On view',
+			// 	'singular_name' => 'On view',
+			// 	'has_archive' => false,
+			// 	'rewrite' => [
+			// 	//'slug' => 'private-sales/private-rooms',
+			// 	'with_front' => false
+			// 	],
+			// 	'menu_icon' =>  'dashicons-admin-site-alt2',
+			// 	'menu_position' => 5,
+			// 	'supports' => [
+			// 		'title',
+			// 		'thumbnail',
+			// 		'revisions'
+			// 	],
+			// ]);
+
+
+		   // Private sales
+        	// create_post_type([
+			// 	'name' => 'Private sales',
+			// 	'singular_name' => 'Private sale',
+			// 	'has_archive' => false,
+			// 	'rewrite' => [
+			// 		//'slug' => 'private-sales/private-rooms',
+			// 		'with_front' => false
+			// 	],
+			// 	'menu_icon' =>  'dashicons-welcome-view-site',
+			// 	'menu_position' => 5,
+			// 	'supports' => [
+			// 		'title',
+			// 		'thumbnail',
+			// 		'revisions'
+			// 	],
+			// ]);
+
          // Private rooms
-        	create_post_type([
-				'name' => 'Private rooms',
-				'singular_name' => 'Private room',
-				'has_archive' => true,
-				'rewrite' => [
-				//'slug' => 'private-sales/private-rooms',
-				'with_front' => false
-				],
-				'menu_icon' =>  'dashicons-hidden',
-				'menu_position' => 10,
-				'supports' => [
-					'title',
-					'thumbnail',
-					'revisions'
-				],
-			]);
+        	// create_post_type([
+			// 	'name' => 'Private rooms',
+			// 	'singular_name' => 'Private room',
+			// 	'has_archive' => true,
+			// 	'rewrite' => [
+			// 	//'slug' => 'private-sales/private-rooms',
+			// 	'with_front' => false
+			// 	],
+			// 	'menu_icon' =>  'dashicons-hidden',
+			// 	'menu_position' => 5,
+			// 	'supports' => [
+			// 		'title',
+			// 		'thumbnail',
+			// 		'revisions'
+			// 	],
+			// ]);
 
 
 
