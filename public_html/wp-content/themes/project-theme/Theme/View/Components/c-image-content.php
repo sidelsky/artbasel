318b0d9ece3edd243e644614b0dd2a230099d7f0
Jim Callender <jimcallender@Jims-Mac-mini.lan>
2020-11-10 14:43:05 +0000

    Plinth AR in image panel on editorial page

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-image-content.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-image-content.php
@@ -1,7 +1,7 @@
 <div class="l-content__block l-content__block--image-content l-content__block--wide-image">
    <div class="canvas l-content__block--center">
 
-      <?php 
+      <?php
       /**
        * If iFrame
       */
@@ -11,7 +11,7 @@
          </div>
       <?php } ?>
 
-      <?php 
+      <?php
       /**
        * If there is a Carousel
          */
@@ -19,22 +19,22 @@
             <button class="fullscreenBtn carouselViewButton target" data-id="fullScreenBtn" title="Fullscreen"></button>
             <button class="closefullscreenBtn carouselViewButton target" data-id="closefullscreenBtn" title="Exit fullscreen"></button>
             <div class="owl-carousel owl-image-content-carousel" >
-               <?php foreach( $data['carousel'] as $image) { ?> 
+               <?php foreach( $data['carousel'] as $image) { ?>
                   <figure class="jq-zoom zoom-image" data-id='carousel-content' role="img" aria-label="<?= esc_attr( $image['alt'] ); ?>" style="background-image: url('<?= $image['sizes']['large'] ?>')">
                      <img style="display:none;" src='<?= $image['sizes']['large'] ?>' alt='<?= esc_attr( $image['alt'] ); ?>'/>
                   </figure>
                <?php } ?>
-            </div>         
+            </div>
       <?php } ?>
-      <?php 
+      <?php
       /**
        * If no video content OR Carousel show a static image
        */
       if( $data['image_content'] && !$data['video_content'] && !$data['carousel'] ) : ?>
-         <figure role="img" aria-label="<?= esc_attr( $data['image_content']['alt'] ); ?>" class="c-video-player__cover-image" style="background-image: url('<?= $data['image_content']['url'] ?>')"></figure>	
+         <figure role="img" aria-label="<?= esc_attr( $data['image_content']['alt'] ); ?>" class="c-video-player__cover-image" style="background-image: url('<?= $data['image_content']['url'] ?>')"></figure>
       <?php endif; ?>
 
-      <?php 
+      <?php
       /**
        * If video content show cover image, play button and video
        */
@@ -44,13 +44,13 @@
                <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#shape-play" viewBox="0 0 32 32"></use>
             </svg>
          </button>
-         <figure class="c-video-player__cover-image" role="img" aria-label="<?= esc_attr( $data['image_content']['alt'] ); ?>" style="background-image: url('<?= $data['image_content']['url'] ?>')" data-id='cover'></figure>	
+         <figure class="c-video-player__cover-image" role="img" aria-label="<?= esc_attr( $data['image_content']['alt'] ); ?>" style="background-image: url('<?= $data['image_content']['url'] ?>')" data-id='cover'></figure>
          <div data-id="vimeo-content" class="u-video-aspect-ratio <?= $data['portrait_video'] ? 'u-video-aspect-ratio--portrait' : 'u-video-aspect-ratio--full-width'?>">
             <?= $data['video_content'] ?>
          </div>
       <?php endif; ?>
    </div>
-   
+
 </div>
 
 <?php
