09858d1d267cc79343e8ccdfc95333b250ed10e0
Jim Callender <jimcallender@Jims-Mac-mini.lan>
2020-11-26 15:01:50 +0000

    add vimeo loop

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-vimeo-hero.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-vimeo-hero.php
@@ -18,8 +18,8 @@
                       'rel'        => 0,
               'modestbranding' => 1,
       				'autoplay' => 1,
-              'muted' => 1
-
+              'muted' => 1,
+              'loop'=1
     );
     $new_src = add_query_arg($params, $src);
     $iframe = str_replace($src, $new_src, $iframe);
