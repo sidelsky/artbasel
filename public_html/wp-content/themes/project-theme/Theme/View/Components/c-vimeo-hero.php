ce0b2220aaa380a02497b6b901d09012dce9ac22
Jim Callender <jimcallender@Jims-Mac-mini.lan>
2020-11-27 15:58:54 +0000

    add pre title to vimeo hero

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-vimeo-hero.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-vimeo-hero.php
@@ -42,6 +42,7 @@
   ?>
 <!-- <img src="<?php echo esc_url( $image['url'] ); ?>" alt="<?php echo esc_attr( $image['alt'] ); ?>" class="show-on-mobile hide" /> -->
   <div id="hero">
+          <p><?php the_sub_field('vimeo_hero_pre_title'); ?></p>
           <h1><?php the_sub_field('vimeo_hero_title'); ?></h1>
           <p><?php the_sub_field('vimeo_hero_desc'); ?></p>
           <a href="<?php echo esc_url( $link ); ?>"><?php echo esc_html( $link_title ); ?></a>
