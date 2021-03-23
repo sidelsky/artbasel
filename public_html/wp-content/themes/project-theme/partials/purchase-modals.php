dd5228e92944595067356385880f8cd3a9e5d723
Jim Callender <jimcallender@Jims-Mac-mini.lan>
2021-03-23 16:08:28 +0000

    price form typos

--- public_html/wp-content/themes/project-theme/partials/purchase-modals.php
+++ public_html/wp-content/themes/project-theme/partials/purchase-modals.php
@@ -8,7 +8,7 @@
 			<svg class="c-header__icon"><use xlink:href="#shape-hauserwirth-logo"></use></svg>
 			<span class="close">&times;</span>
 			<?= do_shortcode('[gravityform id="5" title="false" description="false" ajax="true" field_values="form_msg=I would like to buy ' . $artworks['fullName'] .', ' . $artworks['title'] . '. \nPlease contact me to finalize the purchase details.&id_code=' . $artworks['ids'] . '"]'); ?>
-			<small>*By submiting your email address, you consent to receive our Newsleter. Your consent is revocable at any time by clicking the unsubscribe link in our Newsleter. The Newsletter is sent in accordance with our Privacy Policy and to advertise products and services of Hauser &amp; Wirth Ltd. and it's afiliated companies.</small>
+			<small>*By submitting your email address, you consent to receive our Newsletter. Your consent is revocable at any time by clicking the unsubscribe link in our Newsletter. The Newsletter is sent in accordance with our Privacy Policy and to advertise products and services of Hauser &amp; Wirth Ltd. and it's affiliated companies.</small>
 		</div>
 	</div>
 <?php endforeach; ?>
@@ -23,7 +23,7 @@
 			<svg class="c-header__icon"><use xlink:href="#shape-hauserwirth-logo"></use></svg>
 			<span class="close">&times;</span>
 			<?= do_shortcode('[gravityform id="5" title="false" description="false" ajax="true" field_values="form_msg=I would like to buy ' . $art['fullName'] .', ' . $art['title'] . '. \nPlease contact me to finalize the purchase details.&id_code=' . $art['ids'] . '"]'); ?>
-			<small>*By submiting your email address, you consent to receive our Newsleter. Your consent is revocable at any time by clicking the unsubscribe link in our Newsleter. The Newsletter is sent in accordance with our Privacy Policy and to advertise products and services of Hauser &amp; Wirth Ltd. and it's afiliated companies.</small>
+			<small>*By submitting your email address, you consent to receive our Newsletter. Your consent is revocable at any time by clicking the unsubscribe link in our Newsletter. The Newsletter is sent in accordance with our Privacy Policy and to advertise products and services of Hauser &amp; Wirth Ltd. and it's affiliated companies.</small>
 		</div>
 	</div>
 <?php endforeach; ?>
