53b0f526624624c43337b1ee6ab559fb8eb9a47f
Jim Callender <jimcallender@Jims-Mac-mini.lan>
2021-03-24 14:38:21 +0000

    remove apostrophe from prices copy

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-inquire-form.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-inquire-form.php
@@ -3,6 +3,6 @@
 		<svg class="c-header__icon"><use xlink:href="#shape-hauserwirth-logo"></use></svg>
 		<span class="close" id="closeBtn">&times;</span>
 		<?= do_shortcode('[gravityform id="' . $args['id'] . '" title="false" description="false" ajax="true"]'); ?>
-		<small>*By submitting your email address, you consent to receive our Newsletter. Your consent is revocable at any time by clicking the unsubscribe link in our Newsletter. The Newsletter is sent in accordance with our Privacy Policy and to advertise products and services of Hauser &amp; Wirth Ltd. and it's affiliated companies.</small>
+		<small>*By submitting your email address, you consent to receive our Newsletter. Your consent is revocable at any time by clicking the unsubscribe link in our Newsletter. The Newsletter is sent in accordance with our Privacy Policy and to advertise products and services of Hauser &amp; Wirth Ltd. and its affiliated companies.</small>
 		</div>
 </div>
