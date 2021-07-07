2e5deabce9ae197aff16893dc1e351003c24261e
jim c <jim@callendercreates.com>
2021-07-07 14:57:06 +0100

    Revert "add vimeo hero to gallery exhibitions template"
    This reverts commit c50a5d79f70c5e1e42bef434dfa7c437c1f67009.

--- public_html/wp-content/themes/project-theme/page-gallery-exhibitions.php
+++ public_html/wp-content/themes/project-theme/page-gallery-exhibitions.php
@@ -27,14 +27,7 @@ include("header.php");
 	</span>
 <?php endif; ?>
 
-<?php if ( get_field('has_vimeo_hero') == true ) { ?>
 
-<!-- Show vimeo hero -->
-<?php get_template_part( 'Theme/View/Components/c-vimeo-hero' ); ?>
-
-<?php } else { ?>
-
-	
 <?php
 
 /**
@@ -79,7 +72,7 @@ include("header.php");
 			echo $render->view('Components/' . $templateName, $value, $args);
 	}
 
-?>
+?> 
 </section>
 
 <?php
