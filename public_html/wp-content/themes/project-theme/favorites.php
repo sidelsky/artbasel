81ddf4093d97c13d7218cde62b2d0fa6bf68e57c
jimcallender <jim@callendercreates.com>
2021-08-19 20:00:01 +0100

    Create favorites.php

---
+++ public_html/wp-content/themes/project-theme/favorites.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Favorites
+ */
+
+include("header-artbasel.php");
+?>
+
+<!-- hero -->
+<section>
+<div class="wrapper favorites>
+ 	<?php echo do_shortcode('[yith_wcwl_wishlist]'); ?>
+</div>
+</section>
+
+
+<?php
+/**
+ * Email submission
+ */
+include("partials/artbasel-email-sub.php"); ?>
+
+
+<?php
+/**
+ * Footer
+ */
+include("footer-artbasel.php"); ?>
