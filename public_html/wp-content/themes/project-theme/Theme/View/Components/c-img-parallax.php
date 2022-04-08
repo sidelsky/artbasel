ff4204381a4920f3a6524d2d4924db8ee036ca77
jimcallender <jim@callendercreates.com>
2022-04-08 15:09:14 +0100

    add css effect to parallax component

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-img-parallax.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-img-parallax.php
@@ -1,7 +1,21 @@
 <?php if( have_rows('content_builder') ): ?>
     <?php while( have_rows('content_builder') ): the_row(); ?>
         <?php if( get_row_layout() == 'kuula1' ): ?>
-          <div class="parallax-window parallax-window__footer" data-parallax="scroll" data-image-src="<?php the_sub_field('kuula_image1'); ?>);"></div>
+          <div class="parallax" style="background-image: url('<?php the_sub_field('kuula_image1'); ?>');"></div>
         <?php endif; ?>
     <?php endwhile; ?>
 <?php endif; ?>
+
+<style>
+.parallax {
+ 
+  /* Set a specific height */
+  min-height: 500px;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
