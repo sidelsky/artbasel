2ac59ce329460e39b4f9149db7aa95e8a17bf53d
jimcallender <jim@callendercreates.com>
2022-04-13 17:39:08 +0100

    update anchor file

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-anchor.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-anchor.php
@@ -1,14 +1,28 @@
-<?php if( $data['widerTextContent'] ) : ?>
-	<section class="u-section u-l-vertical-padding--margin-40">
-		<div class="u-l-container<?= $args['alignLeft'] ? '--1150' : '--shallow'?> u-l-horizontal-padding--medium l-content__block__text-content--wide <?= $args['alignLeft'] ? 'u-l-container--no-margin' : ''?>">
-            <div class="l-content__block__text-content l-content__block__body-text" id="<?= $data['widerTextAnchor'] ?>">
-               <?php if( $data['widerTextTitle'] ) { ?>
-
-                  <h2 class="l-content__block__title <?= $args['altFontClass'] ? 'l-content__block__title--alt-font' : ''?>"><?= $data['widerTextTitle'] ?></h2>
-               <?php } ?>
-
-							 <?= $data['widerTextContent'] ?>
-            </div>
-		</div>
-   </section>
+<?php if( have_rows('content_builder') ): ?>
+    <?php while( have_rows('content_builder') ): the_row(); ?>
+        <?php if( get_row_layout() == 'anchor' ): ?>
+					<section class="u-section u-l-vertical-padding--margin-40 anchor-wrap">
+						<div class="u-l-container--shallow u-l-horizontal-padding--medium l-content__block__text-content--wide ">
+							<div class="l-content__block__text-content l-content__block__body-text">
+                   <h2 class="l-content__block__title" >
+										 <?php the_sub_field('anchor_title'); ?>
+									 </h2>
+								 </div>
+								 </div>
+								 <div class="anchor">
+									 <?php if( have_rows('anchor_link') ): ?>
+ 									 <ul class="link">
+									 	<?php while ( have_rows('anchor_link') ) : the_row(); ?>
+									    <li>
+													<a href="#<?php the_sub_field('url'); ?>">
+														<?php the_sub_field('title'); ?>
+													</a>
+											 </li>
+												    <?php endwhile; ?>
+												    </ul>
+												<?php endif; ?>
+											</div>
+						</section>
+	   		<?php endif; ?>
+    <?php endwhile; ?>
 <?php endif; ?>
