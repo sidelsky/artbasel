b286af4d0e4fddf42b35ce1ef750d736ee2a5c33
jimcallender <jim@callendercreates.com>
2022-07-28 19:30:29 +0100

    reinstate anchior video

--- public_html/wp-content/themes/project-theme/Theme/View/Components/c-anchor.php
+++ public_html/wp-content/themes/project-theme/Theme/View/Components/c-anchor.php
@@ -1,9 +1,31 @@
 <?php if( have_rows('content_builder') ): ?>
     <?php while( have_rows('content_builder') ): the_row(); ?>
         <?php if( get_row_layout() == 'anchor' ): ?>
+          <div class="l-content__block l-content__block--image-content l-content__block--wide-image">
+             <div class="canvas l-content__block--center">
+
+
+                <?= $data['coverimage'] ?>
+                <?= $data['video'] ?>
+                <?= $data['videotitle'] ?>
+                <?= $data['videodesc'] ?>
+
+
+								 </div>
+								 </div>
+
+	   		<?php endif; ?>
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<?php if( have_rows('content_builder') ): ?>
+    <?php while( have_rows('content_builder') ): the_row(); ?>
+        <?php if( get_row_layout() == 'anchor' ): ?>
 					<section class="u-section u-l-vertical-padding--margin-40 anchor-wrap">
 						<div class="u-l-container--shallow u-l-horizontal-padding--medium l-content__block__text-content--wide ">
 							<div class="l-content__block__text-content l-content__block__body-text" id="anchor-top">
+
+
                    <h2 class="l-content__block__title" >
 										 <?php the_sub_field('anchor_title'); ?>
 									 </h2>
